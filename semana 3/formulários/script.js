ed6944745765c27cf8aded576a9d00cacab5076b
Jamile Fernandes <jamile.fern4ndes@gmail.com>
2025-10-07 20:29:39 -0300

    acréscimo das validações

--- semana 3/formulários/script.js
+++ semana 3/formulários/script.js
@@ -8,28 +8,50 @@ let users = [
     }
 ]
 
+//verificação envio do formulário
 document.querySelector('#submit').addEventListener('click', (e) => {
     e.preventDefault()
 
+    //Captura dos dados 
     const nome      = document.querySelector('#primeiroNome').value
     const sobrenome = document.querySelector('#sobrenome').value
     const email     = document.querySelector('#email').value
     const telefone  = document.querySelector('#telefone').value
     const senha1    = document.querySelector('#senha1').value
     const senha2    = document.querySelector('#senha2').value
+    let rule = document.querySelector('.rule')
 
+    //salvando no array
     users[0].Nome      = nome
     users[0].Sobrenome = sobrenome
-    users[0].Email     = email
-    users[0].Telefone  = telefone
 
-    if (senha1 === senha2) {
-        users[0].Senha = senha1
-        console.log(users)
-    } else {
-        console.log('senha incorreta')
+    //validação do email
+    if(!email.includes('@')){
+        rule.innerHTML += 'Email inválido  <br>'
+    }else{
+        users[0].Email     = email
+        rule.innerHTML = ''
     }
 
+    //validação do telefone
+    if (telefone.lenght != 11){
+        rule.innerHTML += 'O Telefone Precisa conter 11 dígitos <br>'
+    }else{
+        users[0].Telefone  = telefone
+        rule.innerHTML = ''
+    }
+
+    //validação das senhas
+    if (senha1 != senha2) { 
+        rule.innerHTML += 'As senhas nâo correspondem <br>'
+        senha1.value = ''
+        senha2.value = ''
+    } else {
+       users[0].Senha = senha1
+       rule.innerHTML = ''
+    }
+
+    //limpeza dos dados formulário
     const inputsText = document.querySelectorAll('.inputText')
     inputsText.forEach(input => input.value = '')
 })
